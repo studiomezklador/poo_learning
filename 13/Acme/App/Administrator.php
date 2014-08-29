02f9a345a0c74c9952445036f3a3cb4275400b97
ZEF on MacPro <se@mezklador.com>
2014-08-29 13:35:14 +0200

    End of lesson 15 : Interfaces, implementations, contract & polymorphism

---
+++ 13/Acme/App/Administrator.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Acme\App;
+
+class Administrator extends User
+{ // !!! The initial '\' is to prepend the library out of the namespace
+
+	public function __construct()
+	{
+		$this->isAdmin = true; // change $isAdmin boolean state
+	}
+
+	public function login()
+	{
+		$message_from_parent = parent::login();
+		return $message_from_parent . ' With Administrator Privileges.';
+	}
+
+	public function logout()
+	{
+		return 'Logged Out from Admin board.';
+	}
+}
---
+++ 13/index.php
@@ -0,0 +1,58 @@
+<!-- JSON -->
+<?php
+// INTERFACES : sharing snippets of code between classes -> a "contract" telling which methods would be sharing between classes
+// See: User, Administrator(child of User), Member (child of User) & index
+
+require 'autoload.php'; // PSR-0 Autoloader
+
+$jsonFile = new Acme\App\Repositories\PostJsonRepository(); // new Obj based on new classes implementing interface
+
+$posts = $jsonFile->All(); // testing: contracted Method -> All()
+
+?>
+<h1>All JSON POSTS</h1>
+<ul>
+  <?php // display all titles from json posts
+    $id = 0;
+    foreach ($posts as $post) {
+      $id++;
+      $title = $post->title;
+      $class = ($id == 2) ? 'style="font-weight:bold;"' : '';
+      echo "<li $class>$title</li>";
+    }
+   ?>
+</ul>
+
+<hr />
+
+<?php $post_two = $jsonFile->Find(2); //testing: contracted Method Find with id=2 ?>
+<h1><?= $post_two->title; ?></h1>
+<p><?= $post_two->body; ?></p>
+
+<hr />
+<!-- XML -->
+
+<?php // different implementations with same functionnality = POLYMORPHISM
+$rssFile = new Acme\App\Repositories\PostRssRepository(); // new Obj based on new classes implementing interface
+
+$posts = $rssFile->All(); // testing: contracted Method -> All()
+?>
+
+<h1>All XML/RSS POSTS</h1>
+<ul>
+  <?php // display all titles from xml posts
+    $id = 0;
+    foreach ($posts as $post) {
+      $id++;
+      $title = $post->title;
+      $class = ($id == 2) ? 'style="font-weight:bold;"' : '';
+      echo "<li $class>$title</li>";
+    }
+   ?>
+</ul>
+
+<hr />
+
+<?php $post_two = $rssFile->Find(2); //testing: contracted Method Find with id=2 ?>
+<h1><?= $post_two->title; ?></h1>
+<p><?= $post_two->body; ?></p>
